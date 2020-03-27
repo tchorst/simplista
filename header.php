be2911a9d8d650b6e25a13d6eb49253fa98da459
Tim <tim@Smartbox1169s-MacBook-Pro.local>
2020-03-27 11:49:16 -0400

    fixing merge issues

--- header.php
+++ header.php
@@ -11,11 +11,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <?php wp_head(); ?>
-<<<<<<< HEAD
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
-=======
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
->>>>>>> bb4951528115f0cb2b12993f7fe1774797cdad0c
     <link rel='stylesheet' id='FontAwesome-css'  href='//use.fontawesome.com/releases/v5.0.13/css/all.css' type='text/css' media='all' />
 </head>
 
@@ -29,42 +25,21 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-<<<<<<< HEAD
       <a class="navbar-brand" href="/"><?php the_custom_logo(); ?></a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <div class="nav navbar-nav navbar-right">
         <?php wp_nav_menu(array ('theme_location' => 'nav-menu','menu_id' => 'nav')); ?>
       </div>
-=======
-      <a class="navbar-brand" href="/"><span class="glyphicon glyphicon glyphicon-chevron-left" aria-hidden="true"></span>tch<span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="https://github.com/tchorst" target="_blank"><i class="fab fa-github"></i></a></li>
-      </ul>
->>>>>>> bb4951528115f0cb2b12993f7fe1774797cdad0c
     </div>
   </div>
 </nav>
 
 <div class="jumbotron">
   <div class="container">
-<<<<<<< HEAD
     <h1 class="headline"><?php bloginfo( 'name' ); ?></h1>
     <p><?php bloginfo( 'description' ); ?></p>
   </div>
 </div>
 
 <div class="container">
-=======
-    <h1 class="headline">Timothy Horst</h1>
-    <p>Development. Site Performance Optimization. DPM.</p>
-  </div>
-</div>
-
-<div class="container">
-    <div class="row">
->>>>>>> bb4951528115f0cb2b12993f7fe1774797cdad0c
